4d96d79356345f3ace05001afbb0d04ec32f650a
Chelsa Wen <129915815+zwen10@users.noreply.github.com>
2023-10-02 03:14:29 -0400

    Update interface.py

--- interface.py
+++ interface.py
@@ -30,7 +30,7 @@ st.header("HELOC Risk Performance Prediction")
 #result
 import urllib.request
 
-url = 'https://github.com/zwen10/streanlit_project/blob/main/boosting_5.p'
+url = 'https://github.com/zwen10/streanlit_project/blob/main/boosting_5.pkl'
 
 with urllib.request.urlopen(url) as f:
     model = pickle.load(f)
